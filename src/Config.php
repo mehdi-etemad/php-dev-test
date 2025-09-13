2cbe1abccb3ceb0a87c2a51aee279935a2e580d8
Mehdi Etemad <mehdi.etemaad@gmail.com>
2025-09-13 13:29:10 -0400

    chore(config): update database connection string for MySQL.

--- src/Config.php
+++ src/Config.php
@@ -4,5 +4,8 @@ namespace silverorange\DevTest;
 
 class Config
 {
-    public string $dsn = 'pgsql:host=localhost;port=5532;dbname=silverorange;user=silverorange;password=silverorange';
+    // public string $dsn = 'pgsql:host=localhost;port=5532;dbname=silverorange;user=silverorange;password=silverorange';
+
+    // the connection is set for a local MySQL Database
+    public string $dsn = 'mysql:host=localhost;dbname=silverorange;user=root;password=';
 }
