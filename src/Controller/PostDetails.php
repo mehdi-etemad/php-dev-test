3c39c06f41dde7720e6e4172f8773bb71d164155
Mehdi Etemad <mehdi.etemaad@gmail.com>
2025-09-15 19:41:22 -0400

    feat(post-details): fetch post from DB, render Markdown as HTML in controller, and design template.

--- src/Controller/PostDetails.php
+++ src/Controller/PostDetails.php
@@ -5,6 +5,7 @@ namespace silverorange\DevTest\Controller;
 use silverorange\DevTest\Context;
 use silverorange\DevTest\Template;
 use silverorange\DevTest\Model;
+use League\CommonMark\CommonMarkConverter;
 
 class PostDetails extends Controller
 {
@@ -19,13 +20,18 @@ class PostDetails extends Controller
     public function getContext(): Context
     {
         $context = new Context();
-
+        $this->loadData();
         if ($this->post === null) {
             $context->title = 'Not Found';
             $context->content = "A post with id {$this->params[0]} was not found.";
         } else {
             $context->title = $this->post->title;
-            $context->content = $this->params[0];
+            $converter = new CommonMarkConverter();
+            $context->content = $converter->convert($this->post->body)->getContent();
+            $context->parameter['id'] = $this->post->id;
+            $context->parameter['created_at'] = date_format(date_create($this->post->created_at), 'Y-m-d');
+            $context->parameter['modified_at'] = date_format(date_create($this->post->modified_at), 'Y-m-d');
+            $context->parameter['author'] = $this->post->author;
         }
 
         return $context;
@@ -51,7 +57,17 @@ class PostDetails extends Controller
 
     protected function loadData(): void
     {
-        // TODO: Load post from database here. $this->params[0] is the post id.
-        $this->post = null;
+        $stmt = $this->db->prepare('SELECT posts.*, authors.full_name FROM posts LEFT JOIN authors ON authors.id=posts.author WHERE posts.id=?');
+        $stmt->execute([$this->params[0]]);
+        $post = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        if($post != null) {
+            $this->post = new Model\Post();
+            $this->post->id = $post[0]['id'];
+            $this->post->title = $post[0]['title'];
+            $this->post->body = $post[0]['body'];
+            $this->post->created_at = $post[0]['created_at'];
+            $this->post->modified_at = $post[0]['modified_at'];
+            $this->post->author = $post[0]['full_name'];
+        }
     }
 }
--- src/Template/PostDetails.php
+++ src/Template/PostDetails.php
@@ -9,7 +9,22 @@ class PostDetails extends Layout
     protected function renderPage(Context $context): string
     {
         return <<<HTML
-            <p>SHOW CONTENT FOR {$context->content} HERE</p>
+            <article class="post">
+                <h1 class="post_title">{$context->title}</h1>
+                
+                <div class="post_meta">
+                    By: <strong>{$context->parameter['author']}</strong>
+                    <br />
+                    Created at: {$context->parameter['created_at']} |
+                    Modified at: {$context->parameter['modified_at']}
+                </div>
+                <div class="post_body">
+                    <p>{$context->content}</p>
+                </div>
+                <div class="post_footer">
+                    Post ID: {$context->parameter['id']}
+                </div>
+            </article>
             HTML;
     }
 }
