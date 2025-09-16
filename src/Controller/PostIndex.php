62efe4d1dc9093e12c781288e311784caaf5955d
Mehdi Etemad <mehdi.etemaad@gmail.com>
2025-09-16 10:33:03 -0400

    feat(posts-index): fetch all posts from DB in controller and design post list template.

--- src/Controller/PostIndex.php
+++ src/Controller/PostIndex.php
@@ -17,7 +17,15 @@ class PostIndex extends Controller
     {
         $context = new Context();
         $context->title = 'Posts';
-        $context->content = strval(count($this->posts));
+        $context->parameter['postsCount'] = count($this->posts);
+        $context->content = '';
+        foreach($this->posts as $post) {
+            $context->content .= '<div class="post-card">
+                    <div class="post-title">'.$post->title.'</div>
+                    <div class="post-author">By: '.$post->author.' ('.date_format(date_create($post->created_at), 'Y-m-d').')</div>
+                    <a class="post-link" href="/posts/'.$post->id.'">Read more ...</a>
+                </div>';
+        }
         return $context;
     }
 
@@ -28,7 +36,18 @@ class PostIndex extends Controller
 
     protected function loadData(): void
     {
-        // TODO: Load posts from database here.
-        $this->posts = [];
+        $stmt = $this->db->prepare('SELECT posts.*, authors.full_name FROM posts LEFT JOIN authors ON authors.id=posts.author ORDER BY posts.created_at DESC');
+        $stmt->execute();
+        $posts = $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        foreach($posts as $postData) {
+            $post = new Post();
+            $post->id = $postData['id'];
+            $post->title = $postData['title'];
+            $post->body = $postData['body'];
+            $post->created_at = $postData['created_at'];
+            $post->modified_at = $postData['modified_at'];
+            $post->author = $postData['full_name'];
+            $this->posts[] = $post;
+        }
     }
 }
--- src/Template/PostIndex.php
+++ src/Template/PostIndex.php
@@ -9,7 +9,11 @@ class PostIndex extends Layout
     protected function renderPage(Context $context): string
     {
         return <<<HTML
-            <p>SHOW ALL {$context->content} POSTS HERE</p>
+            <h1>ALL {$context->parameter['postsCount']} POSTS</h1>
+            <div class="post-list">
+                {$context->content}
+            </div>
             HTML;
+        return $mainContext;
     }
 }
